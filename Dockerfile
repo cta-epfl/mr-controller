4a32c2238cb8b2763b0721ebc3e7f11f14007a00
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-09-21 11:03:45 +0200

    Initial commit using simple structure

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,20 @@
-# Build the manager binary
-FROM golang:1.19 as builder
-ARG TARGETOS
-ARG TARGETARCH
+FROM debian:bookworm-slim
 
-WORKDIR /workspace
-# Copy the Go Modules manifests
-COPY go.mod go.mod
-COPY go.sum go.sum
-# cache deps before building and copying source so that we don't need to re-download as much
-# and so that source changes don't invalidate our downloaded layer
-RUN go mod download
+LABEL author=ph0tonic
+ENV UID=1000
 
-# Copy the go source
-COPY main.go main.go
-COPY api/ api/
-COPY controllers/ controllers/
+RUN apt-get update && apt-get install -y \
+    git \
+    curl \
+    bash \
+  && rm -rf /var/lib/apt/lists/*
 
-# Build
-# the GOARCH has not a default value to allow the binary be built according to the host where the command
-# was called. For example, if we call make docker-build in a local env which has the Apple Silicon M1 SO
-# the docker BUILDPLATFORM arg will be linux/arm64 when for Apple x86 it will be linux/amd64. Therefore,
-# by leaving it empty we can ensure that the container and binary shipped on it will have the same platform.
-RUN CGO_ENABLED=0 GOOS=${TARGETOS:-linux} GOARCH=${TARGETARCH} go build -a -o manager main.go
+RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" \
+  && install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl \
+  && rm kubectl
 
-# Use distroless as minimal base image to package the manager binary
-# Refer to https://github.com/GoogleContainerTools/distroless for more details
-FROM gcr.io/distroless/static:nonroot
-WORKDIR /
-COPY --from=builder /workspace/manager .
-USER 65532:65532
+RUN [ -x /usr/sbin/useradd ] && useradd -m -u ${UID} ephemeral-controller -s /bin/bash
 
-ENTRYPOINT ["/manager"]
+USER ${UID}}
+
+ENTRYPOINT [ "bash" ]
--- main.go
+++ main.go
@@ -1,115 +1,61 @@
-/*
-Copyright 2023.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"flag"
-	"os"
+	"fmt"
+	"log"
+	"time"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	appsv1alpha1 "github.com/cta-epfl/mr-controller/api/v1alpha1"
-	"github.com/cta-epfl/mr-controller/controllers"
-	//+kubebuilder:scaffold:imports
+	"github.com/xanzy/go-gitlab"
 )
 
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
+func loop(git *gitlab.Client){
+	targetBranch := "main"
+	openedState := "opened"
+	merge_requests, _, err := git.MergeRequests.ListProjectMergeRequests("porjectId", &gitlab.ListProjectMergeRequestsOptions{
+		TargetBranch: &targetBranch,
+		State: &openedState,
+	})
 
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	if err == nil {
+		// TODO: Manage nill
+	}
 
-	utilruntime.Must(appsv1alpha1.AddToScheme(scheme))
-	//+kubebuilder:scaffold:scheme
+	// TODO Identify new merge_requests
+
 }
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
+    fmt.Println("Hello, world.")
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	// TODO: implement
+	// 1. Get open MR
+	// 2. Generate Helm Charts
+	// 3. Loop
+	// 4. Detect closed MR
+	// 5. Detect opened MR
+	// 6. Update edited MR
+	// 7. Messages on GitLab MR
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "45f36689.my.domain",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
+	gitlab_token := "gitlab_token"
+	git, err := gitlab.NewClient(gitlab_token)
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	if err = (&controllers.FeatureMrControllerReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "FeatureMrController")
-		os.Exit(1)
-	}
-	//+kubebuilder:scaffold:builder
+	ticker := time.NewTicker(5 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+		   select {
+			case <- ticker.C:
+				loop(git)
+			case <- quit:
+				ticker.Stop()
+				return
+			}
+		}
+	 }()
+	 
 
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
 
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
-	}
 }
