c9b6c5ae0d11ac8a479aca2fa46c84998673d9ab
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-09-21 11:50:34 +0200

    WIP interact with kluster

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/xanzy/go-gitlab"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
-func loop(git *gitlab.Client){
+func loop(clientset *kubernetes.Clientset, git *gitlab.Client){
+	
+	// TODO: implement
+	// 1. Get open MR
+	// 2. Detect closed MR
+	// 2a. Delete environement
+	// 3. Detect opened MR
+	// 3a. Generate Helm Charts
+	// 4. Update edited MR
+	// 4a. Update environement
+	// 5. Messages on GitLab MR
+
 	targetBranch := "main"
 	openedState := "opened"
 	merge_requests, _, err := git.MergeRequests.ListProjectMergeRequests("porjectId", &gitlab.ListProjectMergeRequestsOptions{
@@ -20,22 +36,71 @@ func loop(git *gitlab.Client){
 		// TODO: Manage nill
 	}
 
-	// TODO Identify new merge_requests
+	var ids []int
+	for _, mr := range merge_requests{
+		ids = append(ids, mr.ID)
+	}
+
+	// Identify new merge_requests
+	// TODO: Load currently deployed namespace
+	_, err = clientset.CoreV1().Namespaces().Get(context.TODO(), "", metav1.GetOptions{})
+
+
+	// Examples for error handling:
+	// - Use helper functions e.g. errors.IsNotFound()
+	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+	} else if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Found example-xxxxx pod in default namespace\n")
+	}
 
 }
 
 func main() {
     fmt.Println("Hello, world.")
 
-	// TODO: implement
-	// 1. Get open MR
-	// 2. Generate Helm Charts
-	// 3. Loop
-	// 4. Detect closed MR
-	// 5. Detect opened MR
-	// 6. Update edited MR
-	// 7. Messages on GitLab MR
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		// get pods in all the namespaces by omitting namespace
+		// Or specify namespace to get pods in particular namespace
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
+		// Examples for error handling:
+		// - Use helper functions e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		_, err = clientset.CoreV1().Pods("default").Get(context.TODO(), "example-xxxxx", metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found example-xxxxx pod in default namespace\n")
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+	
 	gitlab_token := "gitlab_token"
 	git, err := gitlab.NewClient(gitlab_token)
 	if err != nil {
@@ -48,7 +113,7 @@ func main() {
 		for {
 		   select {
 			case <- ticker.C:
-				loop(git)
+				loop(clientset, git)
 			case <- quit:
 				ticker.Stop()
 				return
